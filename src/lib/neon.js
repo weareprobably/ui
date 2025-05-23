29e7f6c9547b307acc43d7679416ce2d2f488f76
Christopher Moody <cmoodychs@gmail.com>
2025-05-22 22:54:23 -0400

    cycle credentials

--- src/lib/neon.js
+++ src/lib/neon.js
@@ -2,8 +2,5 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "../db/schema.js";
 
-const sql = neon(
-  "postgresql://dev_owner:npg_7YW5XcLHAdrC@ep-frosty-meadow-a4crm3xt-pooler.us-east-1.aws.neon.tech/dev?sslmode=require"
-);
-// const sql = neon(process.env.NEON_DATABASE_URL);
+const sql = neon(process.env.NEON_DATABASE_URL);
 export const db = drizzle({ schema, client: sql });
