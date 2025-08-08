7dd8d887b6b1dc5fb047c2432549e8dc6533d160
Christopher Moody <cmoodychs@gmail.com>
2025-08-08 12:10:52 -0400

    whoa this is too big

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import { z } from "astro:schema";
 import db from "../db";
 import { posts, authorOf, postedIn } from "../db/schema.js";
 import generateId from "../lib/generateId.js";
+import Airtable from "airtable";
 
 export const server = {
   getGreeting: defineAction({
@@ -22,6 +23,27 @@ export const server = {
       await db.insert(posts).values(newPost);
       await db.insert(authorOf).values({ postId: postId, userId: input.authorId });
       await db.insert(postedIn).values({ postId: postId, channelId: input.channelId });
+      return true;
+    },
+  }),
+  signUp: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.string(),
+      subscribe: z.boolean(),
+    }),
+    handler: async (input) => {
+      var base = new Airtable({ apiKey: import.meta.env.AIRTABLE_KEY }).base("appHiscTVrOzRAG7x");
+
+      base("Signups").create([
+        {
+          fields: {
+            fldEN63o9N1knNdCr: "foo@bar.com",
+            fldWSCWOu8Qqd28rq: true,
+          },
+        },
+      ]);
+
       return true;
     },
   }),
